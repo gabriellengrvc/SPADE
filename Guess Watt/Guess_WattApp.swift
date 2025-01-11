267dbc163cd6f0a89ec0b6ee33abd770957cbb44
gabriellengrvc <gabriellenguyengervacio@gmail.com>
2025-01-11 15:28:34 +0100

    Initial Commit

---
+++ Guess Watt/Guess_WattApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_WattApp.swift
+//  Guess Watt
+//
+//  Created by Gabrielle on 11.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Guess_WattApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
